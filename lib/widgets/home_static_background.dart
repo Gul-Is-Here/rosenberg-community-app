896b9cfabfe9fc22af9d0dc9618599a778eee62b
Gul-Is-Here <gulfarazahmed58@gmail.com>
2024-09-17 16:08:37 -0500

    'Home Redesign Working'

--- lib/widgets/home_static_background.dart
+++ lib/widgets/home_static_background.dart
@@ -1,84 +1,159 @@
-import 'package:community_islamic_app/constants/color.dart';
-import 'package:community_islamic_app/controllers/home_controller.dart';
+import 'dart:math';
+import 'package:community_islamic_app/views/about_us/about_us.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
 import 'package:get/get.dart';
+import 'package:community_islamic_app/controllers/home_controller.dart';
+import 'package:community_islamic_app/controllers/qibla_controller.dart';
+import 'package:community_islamic_app/constants/color.dart';
+import 'package:community_islamic_app/constants/image_constants.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-import '../constants/image_constants.dart';
-
 class HomeStaticBackground extends StatelessWidget {
-  HomeStaticBackground({
-    super.key,
-    required this.screenHeight,
-  });
+  HomeStaticBackground({super.key, required this.screenHeight});
 
   final double screenHeight;
 
   @override
   Widget build(BuildContext context) {
-    // Get the HomeController instance using Get.find to ensure we don't create multiple instances
+    final screenWidth = MediaQuery.of(context).size.width;
     final homeController = Get.find<HomeController>();
-    final screenHeight1 = MediaQuery.of(context).size.height;
+    final qiblahController = Get.put(QiblahController());
 
-    return screenHeight1 < 700
-        ? Container(
-            height: screenHeight * 0.24,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(homeBg),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Column(
-              children: [
-                VxSwiper.builder(
-                  autoPlayCurve: Curves.easeInCirc,
-                  autoPlay: true,
-                  viewportFraction: .8,
-                  height: 120,
-                  itemCount: bannerList.length,
-                  itemBuilder: (context, index) {
-                    return Image.asset(bannerList[index])
-                        .box
-                        .rounded
-                        .color(asColor)
-                        .make()
-                        .p16();
-                  },
-                ),
-              ],
-            ),
+    return Container(
+      height: screenHeight * 0.24,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(homeBg),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: _buildDateAndCompassExpanded(homeController)),
+              Expanded(
+                  child: _buildQiblahIndicator(
+                      context, screenWidth, qiblahController)),
+            ],
           )
-        : Container(
-            height: screenHeight * 0.24,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(homeBg),
-                fit: BoxFit.cover,
-              ),
-            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDateAndCompassExpanded(HomeController homeController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                10.heightBox,
-                VxSwiper.builder(
-                  autoPlayCurve: Curves.easeInCirc,
-                  autoPlay: true,
-                  viewportFraction: .8,
-                  height: 160,
-                  itemCount: bannerList.length,
-                  itemBuilder: (context, index) {
-                    return Center(
-                      child: Text(
-                        "Index $index",
-                        style: const TextStyle(color: Colors.white),
+                Obx(() {
+                  if (homeController.prayerTime.value.data != null) {
+                    final gregorian =
+                        homeController.prayerTime.value.data!.date.gregorian;
+                    final hijri =
+                        homeController.prayerTime.value.data!.date.hijri;
+
+                    return Text(
+                      '${gregorian.month.en} ${gregorian.day}, ${gregorian.year}\n'
+                      '${hijri.weekday.en} ${hijri.day} ${hijri.month.en} ${hijri.year}',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 9,
+                        color: Colors.white,
+                        fontFamily: popinsRegulr,
                       ),
-                    ).box.rounded.color(asColor).make().p16();
-                  },
+                    );
+                  } else {
+                    return const Text('');
+                  }
+                }),
+                Text(
+                  'Next Prayer Time',
+                  style: TextStyle(color: asColor, fontFamily: popinsSemiBold),
                 ),
+                Text(
+                  '(${homeController.getCurrentPrayer()})',
+                  style: TextStyle(color: asColor, fontFamily: popinsSemiBold),
+                ),
+                10.heightBox,
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.timelapse,
+                      color: Colors.white,
+                    ),
+                    5.widthBox,
+                    Text(
+                      homeController.formatPrayerTime(
+                        homeController.getPrayerTimes().toString(),
+                      ),
+                      style: const TextStyle(
+                          color: Colors.white, fontFamily: popinsBold),
+                    )
+                  ],
+                )
               ],
             ),
-          );
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQiblahIndicator(
+      BuildContext context, double screenWidth, QiblahController controller) {
+    return Expanded(
+      child: StreamBuilder<QiblahDirection>(
+        stream: FlutterQiblah.qiblahStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(color: Color(0xFF006367)),
+            );
+          }
+
+          if (snapshot.hasData) {
+            final qiblahDirection = snapshot.data!;
+            controller.animation = Tween(
+              begin: controller.begin,
+              end: (qiblahDirection.qiblah * (pi / 180) * -1),
+            ).animate(controller.animationController);
+            controller.begin = (qiblahDirection.qiblah * (pi / 180) * -1);
+            controller.animationController.forward(from: 0);
+
+            return Center(
+              child: AnimatedBuilder(
+                animation: controller.animation,
+                builder: (context, child) => Transform.rotate(
+                  angle: controller.animation.value,
+                  child: Image.asset(
+                    compass,
+                    height: screenHeight * 0.2,
+                    width: screenWidth * 0.5,
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return Center(
+              child: const Text(
+                "Unable to get Qiblah direction,\n       Please restart the app",
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
